d30aec6394e989b69329c10482336e27ea479169
tracey brimage <onelovenilynn@gmail.com>
2019-10-06 06:14:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog(name)
+
+  name=["fido","lassie","lassie"]
+end
+
+:fido = Dog.new
+:snoopy = Dog.new
+:lassie = Dog.new
+
+Dog::name
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person(name)
+  adele_goldberg = Person.new
+
+  alan_kay = Person.new
+
+end
+name::Person
+end 

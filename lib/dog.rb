9c9c99f59b54eb50792ec1811590543e4731071f
tracey brimage <onelovenilynn@gmail.com>
2019-10-06 06:05:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,6 @@
 class Dog(name)
 end
 
-fido = Dog.new
-snoopy = Dog.new
-lassie = Dog.new
-
-fido.name="fido"
-snoopy.name="snoopy"
-lassie.name="lassie"
+:fido = Dog.new
+:snoopy = Dog.new
+:lassie = Dog.new
